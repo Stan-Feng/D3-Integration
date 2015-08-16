5f2e4d7dede5c4eb3ecaede38c01760483ccc69a
fengjunwenstan <fengjunwenstan@163.com>
2015-08-16 12:54:01 +0800

    Basic Linechar with lables and path

---
+++ public/scripts/basicLineChar.js
@@ -0,0 +1,46 @@
+(function(){
+  var monthlySales = [
+     {"month":10, "sales":100},
+     {"month":20, "sales":130},
+     {"month":30, "sales":250},
+     {"month":40, "sales":300},
+     {"month":50, "sales":265},
+     {"month":60, "sales":225},
+     {"month":70, "sales":180},
+     {"month":80, "sales":120},
+     {"month":90, "sales":145},
+     {"month":100, "sales":130}
+   ];
+  var width = 500;
+  var height = 350;
+
+  var lineFun = d3.svg.line()
+        .x(function(d){ return d.month * 4; })
+        .y(function(d){ return height - d.sales; })
+        .interpolate('linear');
+
+  var svg = d3.select('#basicLineChar').append('svg')
+        .attr({
+          width: width,
+          height: height
+        });
+
+  var viz = svg.append('path')
+        .attr({
+          d: lineFun(monthlySales),
+          stroke: 'purple',
+          'stroke-width': 2,
+          fill: 'none'
+        });
+
+  var labels = svg.selectAll('text')
+        .data(monthlySales)
+        .enter()
+        .append('text')
+          .text(function(d){ return d.sales;} )
+          .attr({
+            x: function(d){ return d.month * 4; },
+            y: function(d){ return height - d.sales}
+          });
+
+})();
