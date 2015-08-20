7c6db172f1ec0e15ccb48bd87faa7484b2ff44a1
fengjunwenstan <fengjunwenstan@163.com>
2015-08-20 13:51:50 +0800

    add path

--- public/scripts/basics/pullTogether.js
+++ public/scripts/basics/pullTogether.js
@@ -1,12 +1,12 @@
 (function(){
 
   //TODO: Set the dimensions of the canvas/graph
-  var margin = { top : 30, bottom : 20, right : 20, left : 30 };
+  var margin = { top : 30, bottom : 40, right : 20, left : 40 };
   var width = 600 - margin.left - margin.right;
   var height = 270 - margin.top - margin.bottom;
 
   //TODO: Parse date / time
-  var parseDate = d3.time.format('%d-%m-%y').parse;
+  var parseDate = d3.time.format('%d-%b-%y').parse;
 
   //TODO: Set the ranges
   var x = d3.time.scale().range([0, width]);
@@ -20,7 +20,7 @@
 
   //TODO: Define the line
   var valueline = d3.svg.line()
-      .x(function(d){ return x(d.dates); })
+      .x(function(d){ return x(d.date); })
       .y(function(d){ return y(d.close); });
 
   //TODO: Add svg canvas
@@ -36,7 +36,39 @@
 
   //TODO: Add dataset
   d3.csv('/public/testdata/data.csv', function(err, data){
+    data.forEach(function(d){
+      d.date = parseDate(d.date);
+      d.close = +d.close;
+    });
     console.log(data);
+
+    //Scale the range
+    x.domain(d3.extent(data, function(d){ return d.date;} ));
+    y.domain([0, d3.max(data, function(d){ return d.close;} )]);
+
+    //Append the valueline path
+    svg.append('path')
+        .attr({
+          'class': 'line',
+          'd': valueline(data)
+        });
+
+    //Add xAxis
+    svg.append('g')
+        .attr({
+          attr: 'x axis',
+          transform: 'translate(0, ' + height + ')'
+        })
+        .call(xAxis);
+
+    //Add yAxis
+    svg.append('g')
+        .attr({
+          attr: 'y axis'
+        })
+        .call(yAxis);
+
+
   });
 
 
