3aa839bd4ef2a6b3c4e04c7e5f3fc4599c28132c
fengjunwenstan <fengjunwenstan@163.com>
2015-08-21 11:03:20 +0800

    Optimize code structure

---
+++ intermediate/scripts/shadow.js
@@ -0,0 +1,111 @@
+(function(){
+
+  var margin = { top : 30, bottom : 40, right : 20, left : 40 };
+  var width = 700 - margin.left - margin.right;
+  var height =  370 - margin.top - margin.bottom;
+
+  var parseDate = d3.time.format('%d-%b-%y').parse;
+  /**
+   * If we want to parse different formats time
+   * All we need TODO:
+   * combina a String "%Y-%m-%d....." from d3 wiki time format
+   * Then invoke it as a function to parse input data then reuturn a Date objetc
+   */
+
+  var x = d3.time.scale().range([0, width]);
+  var y = d3.scale.linear().range([height, 0]);
+
+  var xAxis = d3.svg.axis().scale(x)
+      .orient('bottom').ticks(5);
+  var yAxis = d3.svg.axis().scale(y)
+      .orient('left').ticks(5);
+
+  var valueline = d3.svg.line()
+      .x(function(d){ return x(d.date); })
+      .y(function(d){ return y(d.close); });
+
+  var svg = d3.select('body').append('svg')
+      .attr({
+        width: width + margin.left + margin.right,
+        height: height + margin.top + margin.bottom
+      }).
+      append('g')
+        .attr({
+          transform: 'translate(' + margin.left + ',' + margin.top + ')'
+        });
+
+  d3.csv('/public/testdata/data.csv', function(err, data){
+    data.forEach(function(d){
+      d.date = parseDate(d.date);
+      d.close = +d.close;
+    });
+
+    //Scale the range
+    x.domain(d3.extent(data, function(d){ return d.date;} ));
+    y.domain([0, d3.max(data, function(d){ return d.close;} )]);
+
+    //Append the valueline path
+    svg.append('path')
+        .attr({
+          'class': 'line',
+          'd': valueline(data)
+        });
+
+    svg.append('g')
+        .attr({
+          class: 'x axis',
+          transform: 'translate(0, ' + height + ')'
+        })
+        .call(xAxis);
+    svg.append('text')
+        .attr({
+          'x': width / 2,
+          'y': height + margin.bottom,
+        })
+        .style('text-anchor', 'center')
+        .text('Date');
+
+
+    svg.append('g')
+        .attr({
+          class: 'y axis'
+        })
+        .call(yAxis);
+    svg.append('text')
+        .attr({
+          'transform': 'rotate(-90)',
+          'y': margin.left / 2,
+          'x': 0 - (height / 2),
+          'dy': '.5em'
+        })
+        .style('text-anchor', 'middle')
+        .text('Value');
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
